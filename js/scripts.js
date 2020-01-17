f78bf50d38e145108556cdd86d9c89c4267755cb
Student <example@example.com>
2020-01-17 10:58:51 -0800

    add the fixing the jquery 'if' statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("form#language").submit(function(event) {
-
+		
 		var animal = $("#animal").val();
 		console.log(animal);
 		var weapon = $("#weapon").val();
@@ -12,8 +12,15 @@ $(document).ready(function() {
 		var house = $("#house").val();
 		console.log(house);
 
-		if ()
-	}
+		if (animal === "lassie" && weapon  === "lightsaber" && friends === "rachael" && cocktail === "margarita" && house === "huffle") {
+			$("#java").show();
+		} else if (animal === "babyYoda" || animal === "lassie" && weapon  === "wizardWand" && friends === "joey" && cocktail === "longIsland" && house === "raven") {
+			$("#csharp").show();
+		} else if (animal === "nagini" || animal === "godzilla" && weapon  === "wizardWand" || weapon === "pokemon" && friends === "joey" && cocktail === "pinaColada" && house === "slyth") {
+			$("#python").show();
+		}
+			
+	
 
 		event.preventDefault();
 	});
