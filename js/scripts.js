dd09592ebb2ee1d3a30b1a91a09f033dba85363e
Student <example@example.com>
2020-01-17 09:43:03 -0800

    add value to each option

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-	
-}
+	$("form#language").submit(function(event) {
+
+		var animal = $("#animal").val();
+		console.log(animal);
+		var weapon = $("#weapon").val();
+		console.log(weapon);
+		var friends = $("#friends").val();
+		console.log(friends);
+		var cocktail = $("#cocktail").val();
+		console.log(cocktail);
+		var house = $("#house").val();
+		console.log(house);
+
+		if ()
+	}
 
 		event.preventDefault();
 	});
