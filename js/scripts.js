d26cb80606e8126e1ede8d3f32681debddd0c1c1
Student <example@example.com>
2020-01-17 14:08:09 -0800

    add a section where users can input their name and the result will include their name but function does not work

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
 		console.log(cocktail);
 		var house = $("#house").val();
 		console.log(house);
+		
+		
+
 
 		if ((animal === "lassie" || animal === "babyYoda") && (weapon  === "lightsaber" || weapon === "yourFists") && (friends === "rachael" || friends === "joey") && cocktail === "margarita" && house === "huffle" || house === "slyth") {
 			$("#csharp").show();
@@ -29,7 +32,15 @@ $(document).ready(function() {
 			$("#java").hide();
 			$("#csharp").hide();
 		}
-			
+		
+	// });
+	// $("form#result").submit(function() {
+	// var userInput = $("input#name").val();
+	// 	$("#name").text(userInput); 
+	// 	console.log(input#name);  
+
+	// 	$(.userName).show();
+
 		event.preventDefault();
 	});
 });
